dc740d035f95ae371cc28e705f44cdfe13312269
Amrik Singh <113277554+Amrik-khaira@users.noreply.github.com>
2022-12-14 04:59:58 +0530

    Add files via upload

---
+++ BMICalculator/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  BMICalculator
+//  Author's name : Amrik Singh
+//  Created by Amrik on 13/12/22.
+//  StudentID : 301296257
+//  BMI Calculator App
+// Version: 1.0
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
